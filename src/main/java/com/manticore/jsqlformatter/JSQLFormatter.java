3ab7d7a2d231cccf87a994fe322df3f58b5539a1
Andreas Reichel <andreas@manticore-projects.com>
2022-09-06 13:47:28 +0700

    chore: use Gradle License plugin

--- src/main/java/com/manticore/jsqlformatter/JSQLFormatter.java
+++ src/main/java/com/manticore/jsqlformatter/JSQLFormatter.java
@@ -1,19 +1,20 @@
-/*
- * Manticore JSQLFormater is a SQL Beautifying and Formatting Software. Copyright (C) 2021 Andreas
- * Reichel <andreas@manticore-rpojects.com>
+/**
+ * Manticore Projects JSQLFormatter is a SQL Beautifying and Formatting Software.
+ * Copyright (C) 2022 Andreas Reichel <andreas@manticore-projects.com>
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU Affero General Public License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License along with this program.
- * If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package com.manticore.jsqlformatter;
 
 import com.diogonunes.jcolor.AnsiFormat;
